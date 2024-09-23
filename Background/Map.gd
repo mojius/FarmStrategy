fbb87ec994d70f9cfb88da3dfe8cd155e05af15c
Ben Daly <mvben33@gmail.com>
2024-09-22 18:06:47 -0700

    Godot 4.3 time

--- Background/Map.gd
+++ Background/Map.gd
@@ -7,5 +7,3 @@ class_name Map extends TileMap
 
 # This constant represents the directions in which a unit can move on the board.
 const DIRECTIONS = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
-
-
